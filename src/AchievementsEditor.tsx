14597e54c31f57a99c17c18ff3e699fb4c33f164
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2024-08-04 23:56:13 -0300

    Make the site a bit more pleasant to the eyes

--- src/AchievementsEditor.tsx
+++ src/AchievementsEditor.tsx
@@ -1,56 +1,64 @@
-
 import { AchievementsType, StateSet } from './util';
+
+import './AchievementsEditor.css';
 
 export function AchievementsEditor(props: {
     achievements: AchievementsType,
     setAchievements: StateSet<AchievementsType>
 }) {
     return (
-        <>
+        <div className='achievements'>
             {
-                Object.entries(props.achievements).map((achievement) => <div>
-                    <h1 key={achievement[0]}>{achievement[0]}</h1>
-                    <h2 key={achievement[0] + '-conds-text'}>Conditions:</h2>
-                    {Object.entries(achievement[1].Conditions).map((condition) => <div key={`${achievement[0]}+${condition[0]}`}>
-                        <h3>{condition[0]}</h3>
+                Object.entries(props.achievements).map((achievement) => <div  key={`${achievement[0]}-div`} className="achievement">
+                    <h2 key={`${achievement[0]}-h2`}>{achievement[0]}</h2>
+                    <div  key={`${achievement[0]}-conditions`} className='conditions'>
 
-                        {
-                            (() => {
-                                const achName = achievement[0];
+                        <h3 key={achievement[0] + '-conds-text'}>Conditions:</h3>
+                        {Object.entries(achievement[1].Conditions).map((condition) => (
+                            <div key={`${achievement[0]}+${condition[0]}`} className='condition'>
+                                <h4>{condition[0]}</h4>
 
-                                const condName = condition[0];
-                                const cond = condition[1];
-                                const key = `${achName}+${condName}`;
+                                <div className='progresses'>
+                                    {
+                                        (() => {
+                                            const achName = achievement[0];
 
-                                const elements = [];
+                                            const condName = condition[0];
+                                            const cond = condition[1];
+                                            const key = `${achName}+${condName}`;
 
-                                if (typeof cond.Value != 'undefined') {
-                                    elements.push(<p>Value:
-                                        <input key={key + '-value'} type='number' value={cond.Value.value} onChange={(ev) => {
-                                            const newAchievements = JSON.parse(JSON.stringify(props.achievements)) as typeof props.achievements;
-                                            if (typeof newAchievements[achName].Conditions[condName].Value === 'undefined') return;
-                                            newAchievements[achName].Conditions[condName].Value.value = parseInt(ev.target.value);
-                                            props.setAchievements(newAchievements);
-                                        }}>
-                                        </input></p>);
-                                }
+                                            const elements = [];
 
-                                elements.push(<p>Completed:
-                                    <input key={key + '-completed'} type='checkbox' checked={cond.Completed} onChange={(_ev) => {
-                                        const newAchievements = JSON.parse(JSON.stringify(props.achievements));
-                                        newAchievements[achName].Conditions[condName].Completed = !newAchievements[achName].Conditions[condName].Completed;
-                                        props.setAchievements(newAchievements);
-                                    }}>
-                                    </input>
-                                </p>);
+                                            if (typeof cond.Value != 'undefined') {
+                                                elements.push(<p key={key + '-value-p'} className='progress'>Value:
+                                                    <input key={key + '-value'} type='number' value={cond.Value.value} onChange={(ev) => {
+                                                        const newAchievements = JSON.parse(JSON.stringify(props.achievements)) as typeof props.achievements;
+                                                        if (typeof newAchievements[achName].Conditions[condName].Value === 'undefined') return;
+                                                        newAchievements[achName].Conditions[condName].Value.value = parseInt(ev.target.value);
+                                                        props.setAchievements(newAchievements);
+                                                    }}>
+                                                    </input></p>);
+                                            }
 
-                                return elements;
-                            })()
-                        }
+                                            elements.push(<p key={key + '-completed-p'} className='progress'>Completed:
+                                                <input key={key + '-completed'} type='checkbox' checked={cond.Completed} onChange={(_ev) => {
+                                                    const newAchievements = JSON.parse(JSON.stringify(props.achievements));
+                                                    newAchievements[achName].Conditions[condName].Completed = !newAchievements[achName].Conditions[condName].Completed;
+                                                    props.setAchievements(newAchievements);
+                                                }}>
+                                                </input>
+                                            </p>);
+
+                                            return elements;
+                                        })()
+                                    }
+                                </div>
+                            </div>
+                        )
+                        )}
                     </div>
-                    )}
                 </div>)
             }
-        </>
+        </div>
     );
 }
