3d06fead51bbee10c12007a949349ab9b7942851
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-03-24 21:19:57 -0300

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-    plugins: [react(), eslint()],
+    plugins: [
+        react(),
+        eslint(),
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: '__tla',
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: (i) => `__tla_${i}`
+        })
+    ],
     server: {
         host: '0.0.0.0'
     },
